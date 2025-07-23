3bbc1667b6be2df1fa5304c01395999b45e44fe7
omega-tom <sontaya390@gmail.com>
2025-07-23 10:05:18 +0700

    delete main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(BarudanLinkApp());
-
-class BarudanLinkApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('BarudanLink')),
-        body: Center(child: Text('Welcome to BarudanLink!')),
-      ),
-    );
-  }
-}
+delete main.dart
